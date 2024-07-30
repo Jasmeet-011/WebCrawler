692932b83387d9aeacc38bea03fc907ed8ceca8c
Jasmeet Singh <jasmeetsingh5003@gmail.com>
2024-07-30 19:38:08 +0530

    created report.js and report.test.js and printed the report on a csv file

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const { crawlPage } = require("./crawl.js");
+const { printReport } = require("./report.js");
 // process.argv lets us get the input from the command line
+
+const { writeDataToCSV } = require("./csv"); // No need to import csvWriter directly
+const path = require("path");
+
+function getFileNameFromUrl(url) {
+  const urlObj = new URL(url);
+  const sanitizedHost = urlObj.hostname
+    .replace(/[^a-z0-9]/gi, "_")
+    .toLowerCase();
+  return `crawled_data_${sanitizedHost}.csv`;
+}
 async function main() {
   // argv is an array of length 3 (name of interpreter, name of the file that is running, url provided)
   if (process.argv.length < 3) {
@@ -11,11 +23,11 @@ async function main() {
     process.exit(1);
   }
   const baseURL = process.argv[2];
+  const csvFileName = getFileNameFromUrl(baseURL);
 
   console.log(`satarting crawl ${baseURL}`);
   const pages = await crawlPage(baseURL, baseURL, {});
-  for (const page of Object.entries(pages)) {
-    console.log(page);
-  }
+  printReport(pages);
+  await writeDataToCSV(pages, path.join(__dirname, csvFileName));
 }
 main();
